9896c235fe9a3c7a2f6194b4339fe27d6c2f7867
Pooja <Pooja@LAPTOP-ISHMI9AQ>
2021-03-15 14:10:47 +0530

    commited gitclass.java

---
+++ GithubProject/src/test/java/GitPackage/Gitclass.java
@@ -0,0 +1,16 @@
+package GitPackage;
+
+import org.testng.annotations.Test;
+
+public class Gitclass {
+	
+	@Test
+	public void lead(){
+		System.out.println("creation of lead");
+	}
+	@Test
+	public void contact(){
+		System.out.println("creation of contact");
+	}
+
+}
